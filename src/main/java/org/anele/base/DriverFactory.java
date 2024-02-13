29dd3ac8232fe61f75be99ad208bbc681ee1b954
Theophelus <aneletom10@gmail.com>
2024-02-13 18:37:13 +0200

    Adding DriverFactory.java to handle multiple browsers and consuming that class on my Hooks.java to get current driver.

--- src/main/java/org/anele/base/DriverFactory.java
+++ src/main/java/org/anele/base/DriverFactory.java
@@ -1,4 +1,81 @@
 package org.anele.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.anele.utils.Constant;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.time.Duration;
+
 public class DriverFactory {
+    protected static WebDriver driver;
+
+    private DriverFactory() {
+        driver = PageDriver.getWebDriver();
+    }
+
+    public static void getWebDriver() throws InterruptedException {
+        if (driver == null) {
+            initBrowser();
+        }
+    }
+
+    public static void initBrowser() throws InterruptedException {
+        //define WebDriver instance
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        ChromeOptions options = new ChromeOptions();
+//        //run tests with desired capabilities
+//        if (Config.browserOptions){
+//            options = getChromeOptions(capabilities);
+//        }
+//        if (browser.equalsIgnoreCase("chrome")){
+//            //get configure browser values
+//            if (Config.browser.equalsIgnoreCase("local")){
+//                WebDriverManager.chromedriver().setup();
+//                driver = new ChromeDriver();
+//                maximizeBrowser(driver);
+//            }else {
+//                if (Config.platform.equalsIgnoreCase("remote")){
+//                    WebDriverManager.chromedriver().setup();
+//                    driver = new ChromeDriver(options);
+//                    maximizeBrowser(driver);
+//                }
+//            }
+//        }else
+//        if (browser.equalsIgnoreCase("edge")){
+
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        maximizeBrowser(driver);
+//        }
+        driver.get(Constant.BASE_URL);
+        Thread.sleep(10000);
+        PageDriver.setWebDriver(driver);
+    }
+
+    //set desired capabilities for chrome options
+    static ChromeOptions getChromeOptions(DesiredCapabilities capabilities) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless=new");
+        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+        options.merge(capabilities);
+        return options;
+    }
+
+    public static void maximizeBrowser(WebDriver driver) {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
---
+++ src/test/java/org/anele/hooks/Hooks.java
@@ -0,0 +1,23 @@
+package org.anele.hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.anele.base.DriverFactory;
+import org.anele.base.PageDriver;
+
+
+public class Hooks {
+    @Before
+    public void setup() throws InterruptedException {
+        DriverFactory.getWebDriver();
+        //set drivers
+        System.out.println("Drivers to be set: " + PageDriver.getWebDriver());
+        System.out.println("Driver instantiated" + PageDriver.getWebDriver().getTitle());
+    }
+
+    @After
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+
+}
