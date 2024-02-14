446db5b2e3afdf9a1139bd8eaab5d751cbd95eed
Theophelus <aneletom10@gmail.com>
2024-02-14 19:42:37 +0200

    updating browser

--- src/main/java/org/anele/base/Config.java
+++ src/main/java/org/anele/base/Config.java
@@ -2,7 +2,7 @@ package org.anele.base;
 
 public class Config {
 
-    public static String browser = System.getProperty("browser", "edge");
+    public static String browser = System.getProperty("browser", "chrome");
     public static String platform = System.getProperty("platform", "local");
     public static boolean browserOptions = Boolean.parseBoolean(System.getProperty("browserOptions", String.valueOf(false)));
 }
