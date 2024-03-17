689b2f59174f873fbe6c4ff99d780a4f80862251
Theophelus <aneletom10@gmail.com>
2024-03-17 14:11:01 +0200

    Adding a method to click flaky elements using javascript

--- src/main/java/org/anele/utils/BaseCore.java
+++ src/main/java/org/anele/utils/BaseCore.java
@@ -14,23 +14,25 @@ import static org.anele.utils.GetWebElements.*;
 
 public class BaseCore {
 
-    public static void setValues(By input, String value){
+    public static void setValues(By input, String value) {
         WebElement element = PageDriver.getWebDriver().findElement(input);
         String value1 = element.getAttribute("value");
         if (!value1.isEmpty()) {
             element.clear();
-        }
-        else
+        } else
             element.sendKeys(value);
     }
-    public static void clickOnBtn(String element){
+
+    public static void clickOnBtn(String element) {
         customWaitForSingleElement(getXpathElement(element), 10);
         getXpathElement(element).click();
     }
-    public static String checkText(String value){
+
+    public static String checkText(String value) {
         return getByName(value).getText();
     }
-    public static boolean checkPartialLink(String link){
+
+    public static boolean checkPartialLink(String link) {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -39,68 +41,73 @@ public class BaseCore {
 
         return PageDriver.getWebDriver().getCurrentUrl().equals(link);
     }
-    public static String validateHeader(String locator){
+
+    public static String validateHeader(String locator) {
 
         return PageDriver.getWebDriver().findElement(By.name(locator)).getText();
     }
-    public static String hasButtonValue(String value){
-        return "//button[normalize-space()='"+ value +"']";
+
+    public static String hasButtonValue(String value) {
+        return "//button[normalize-space()='" + value + "']";
     }
-    public static String hasLinkValue(String value){
-        return "//a[normalize-space()='" +value+ "']";
+
+    public static String hasLinkValue(String value) {
+        return "//a[normalize-space()='" + value + "']";
     }
 
     //define an explicit wait method
-    public static void customWaitForAListOfElements(List<WebElement> element, int waitInSeconds){
+    public static void customWaitForAListOfElements(List<WebElement> element, int waitInSeconds) {
         WebDriverWait wait = new WebDriverWait(PageDriver.getWebDriver(), Duration.ofSeconds(waitInSeconds));
         wait.until(ExpectedConditions.visibilityOfAllElements(element));
     }
 
-    public static void filterList(List<WebElement> elements, String value){
+    public static void filterList(List<WebElement> elements, String value) {
 
-        if (!elements.isEmpty()){
-            for (WebElement list: elements){
-                if (list.isDisplayed() && list.getText().equals(value)){
+        if (!elements.isEmpty()) {
+            for (WebElement list : elements) {
+                if (list.isDisplayed() && list.getText().equals(value)) {
                     list.click();
                     break;
                 }
             }
-        }
-        else
-            throw new NoSuchElementException("Provided "+value+" can not be located");
+        } else
+            throw new NoSuchElementException("Provided " + value + " can not be located");
     }
-    public static void customWaitForSingleElement(WebElement element, int waitInSeconds){
+
+    public static void customWaitForSingleElement(WebElement element, int waitInSeconds) {
         WebDriverWait wait = new WebDriverWait(PageDriver.getWebDriver(), Duration.ofSeconds(waitInSeconds));
         wait.until(ExpectedConditions.visibilityOf(element));
     }
-    public static void clickBtn(String btn){
+
+    public static void clickBtn(String btn) {
         boolean displayed = getXpathElement(hasButtonValue(btn)).isDisplayed();
 
-        try{
+        try {
             if (displayed) {
                 getXpathElement(hasButtonValue(btn)).click();
             }
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void clickLinks(String value){
+    public static void clickLinks(String value) {
         WebElement xpathElement = getXpathElement(value);
         try {
-            if (xpathElement.getText().equalsIgnoreCase(value)){
+            if (xpathElement.getText().equalsIgnoreCase(value)) {
                 xpathElement.click();
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-    public static void waitForElementToBeClicked(WebElement element, int timeInSeconds){
+    public static void waitForElementToBeClicked(WebElement element, int timeInSeconds) {
         WebDriverWait wait = new WebDriverWait(PageDriver.getWebDriver(), Duration.ofSeconds(timeInSeconds));
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
+
     public static String getText(String element) throws Exception {
         try {
 
@@ -113,17 +120,18 @@ public class BaseCore {
             throw new Exception("Validation failed: " + e.getMessage());
         }
     }
+
     public static void enter(String element) throws Exception {
 
         WebElement element1 = getXpathElement(element);
 
-        try{
+        try {
             if (element1.isDisplayed())
-            element1.sendKeys(Keys.ENTER);
+                element1.sendKeys(Keys.ENTER);
             else
                 element1.click();
 
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             throw new Exception("Validation failed: " + e.getMessage());
         }
@@ -132,35 +140,36 @@ public class BaseCore {
     public static void setRequiredField(WebElement webElement,
                                         String userInput) throws Exception {
         String valueAttribute = getValue(webElement);
-       try{
-           if (!valueAttribute.isEmpty()){
-               webElement.sendKeys(Keys.BACK_SPACE.toString().repeat(valueAttribute.length()));
-           }
-           webElement.sendKeys(userInput);
+        try {
+            if (!valueAttribute.isEmpty()) {
+                webElement.sendKeys(Keys.BACK_SPACE.toString().repeat(valueAttribute.length()));
+            }
+            webElement.sendKeys(userInput);
 
-       }catch (Exception e){
-           String message = e.getMessage();
-           System.out.println("Value is not available: " + message);
-       }
+        } catch (Exception e) {
+            String message = e.getMessage();
+            System.out.println("Value is not available: " + message);
+        }
     }
-    public static String getValue(WebElement element){
+
+    public static String getValue(WebElement element) {
         return element.getAttribute("value");
     }
 
-    public static String validateLinks(WebElement element){
+    public static String validateLinks(WebElement element) {
 
         WebElement linkToBeVerified = waitForElementPresent(element);
         try {
             if (linkToBeVerified.isDisplayed())
                 return linkToBeVerified.getText();
 
-        } catch (Exception e){
-            System.out.printf("Link to be verified "+element.getText()+" can't be found "+e.getMessage()+"");
+        } catch (Exception e) {
+            System.out.printf("Link to be verified " + element.getText() + " can't be found " + e.getMessage() + "");
         }
         return null;
     }
 
-    public static WebElement waitForElementPresent(WebElement element){
+    public static WebElement waitForElementPresent(WebElement element) {
         WebDriverWait wait = new WebDriverWait(PageDriver.getWebDriver(), Duration.ofSeconds(30));
         return wait.until(ExpectedConditions.visibilityOf(element));
     }
@@ -174,6 +183,14 @@ public class BaseCore {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void clickElementsUsingJavascript(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor)
+                PageDriver.getWebDriver();
+        waitForElementToBeClicked(element, 10);
+        js.executeScript("arguments[0].click();", element);
+
     }
 }
 
