aab5aa60dae4ef4fbdef591c08d77c52a57bfec3
Theophelus <aneletom10@gmail.com>
2024-03-17 20:04:36 +0200

    adding methods filter records and select based on filters

--- src/main/java/org/anele/pages/EmployeeListPage.java
+++ src/main/java/org/anele/pages/EmployeeListPage.java
@@ -1,14 +1,63 @@
 package org.anele.pages;
 
+import org.anele.utils.BaseCore;
 import org.anele.utils.GetWebElements;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class EmployeeListPage {
-    private String employeeListTab = "//a[normalize-space()='Employee List']";
-    public boolean validateEmployeeListTabPreselected(){
+    private String employeeListTab = "//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]";
+    private String recordsFound = "//div[@class=\"orangehrm-horizontal-padding orangehrm-vertical-padding\"]/span";
+    private String personalDetailsHeader = "//h6[normalize-space()='Personal Details']";
+    public boolean validateEmployeeListTabPreselected() {
         WebElement element = GetWebElements.getXpathElement(employeeListTab);
-        return element.getAttribute("class").contains("active");
+        BaseCore.waitForElementPresent(element);
+        return element.getAttribute("class").contains("visited");
+    }
+
+    public Boolean validateNumberOfRecords(String value) throws Exception {
+
+        WebElement xpathElement = GetWebElements.getXpathElement(recordsFound);
+        return xpathElement.getText().contains(value) ? true : null;
 
     }
 
+    public void clickEditOnSelectedRecord(String value) throws InterruptedException {
+        String leftNextButton = "//i[@class=\"oxd-icon bi-chevron-right\"]";
+        List<WebElement> elementForNextBtn = GetWebElements.getWebElementsByXpath(leftNextButton);
+        String listOfRecords = "//div[@class=\"oxd-table-card\"]//div[normalize-space()= '"+value+"']//following-sibling::div";
+        List<WebElement> elementList = GetWebElements.getWebElementsByXpath(listOfRecords);
+
+        System.out.println("Return true: 1 " +!elementList.isEmpty());
+
+        boolean controlLoop = false;
+
+        while (!controlLoop) {
+
+            if (!elementList.isEmpty()) controlLoop = true;
+
+            else if (!elementForNextBtn.isEmpty()) {
+                WebElement clickNextBtn = GetWebElements.getXpathElement("//i[@class=\"oxd-icon bi-chevron-right\"]");
+                clickNextBtn.click();
+                Thread.sleep(5000);
+            }else {
+                System.out.println("Record not found:" + value);
+                controlLoop = true;
+            }
+
+            WebElement clickEditBtn = GetWebElements.getXpathElement("//div[@class=\"oxd-table-card\"]//div[normalize-space()= '"+value+"']//following-sibling::div//button[2]");
+            if (clickEditBtn !=null){
+                System.out.println("To be clicked: "+clickEditBtn.isDisplayed());
+                clickEditBtn.click();
+                controlLoop = true;
+            }
+        }
+
+
+    }
+
+    public String validatePersonalDetailsHeader() throws Exception {
+       return BaseCore.getText(personalDetailsHeader);
+    }
 }
