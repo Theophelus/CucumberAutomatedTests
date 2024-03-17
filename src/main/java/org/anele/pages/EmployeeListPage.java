a1d3324a74cb205f3d84577411d88cf84297a695
Theophelus <aneletom10@gmail.com>
2024-03-17 14:21:17 +0200

    adding a method to validate Employee list tab being preselected on PIM page

--- src/main/java/org/anele/pages/EmployeeListPage.java
+++ src/main/java/org/anele/pages/EmployeeListPage.java
@@ -1,5 +1,14 @@
 package org.anele.pages;
 
+import org.anele.utils.GetWebElements;
+import org.openqa.selenium.WebElement;
+
 public class EmployeeListPage {
+    private String employeeListTab = "//a[normalize-space()='Employee List']";
+    public boolean validateEmployeeListTabPreselected(){
+        WebElement element = GetWebElements.getXpathElement(employeeListTab);
+        return element.getAttribute("class").contains("active");
+
+    }
 
 }
