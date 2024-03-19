c4e80c2d14c6321e6a2d2972f2d2e2d3ab7ce14b
Theophelus <aneletom10@gmail.com>
2024-03-19 15:26:45 +0200

    adding setters and getter for AddEmployee.java and LoginDetails.java class

--- src/main/java/org/anele/models/employees/AddEmployee.java
+++ src/main/java/org/anele/models/employees/AddEmployee.java
@@ -2,5 +2,57 @@ package org.anele.models.employees;
 
 public class AddEmployee {
 
+    private String firstname;
+    private String middleName;
+    private String lastname;
+    private int employeeId;
+    private LoginDetails loginDetails;
 
+    public AddEmployee(String firstname, String middleName, String lastname, int employeeId, LoginDetails loginDetails) {
+        this.firstname = firstname;
+        this.middleName = middleName;
+        this.lastname = lastname;
+        this.employeeId = employeeId;
+        this.loginDetails = loginDetails;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public LoginDetails getLoginDetails() {
+        return loginDetails;
+    }
+
+    public void setLoginDetails(LoginDetails loginDetails) {
+        this.loginDetails = loginDetails;
+    }
 }
--- src/main/java/org/anele/models/employees/LoginDetails.java
+++ src/main/java/org/anele/models/employees/LoginDetails.java
@@ -1,4 +1,48 @@
 package org.anele.models.employees;
 
 public class LoginDetails {
+
+    private String username;
+    private String status;
+    private String password;
+    private String confirmPassword;
+
+    public LoginDetails(String username, String status, String password, String confirmPassword) {
+        this.username = username;
+        this.status = status;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
